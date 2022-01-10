de5844f4da16f3d6e29bf5fccef1d2d525e4f939
unknown <leontyloz02@gmail.com>
2022-01-10 15:42:57 +0300

    Add FileCabinetRecord.cs file.

---
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -0,0 +1,13 @@
+﻿namespace FileCabinetApp
+{
+  public class FileCabinetRecord
+    {
+        public int Id { get; set; }
+
+        public int FirstName { get; set; }
+
+        public int LastName { get; set; }
+
+        public int DateOfBirth { get; set; }
+    }
+}
