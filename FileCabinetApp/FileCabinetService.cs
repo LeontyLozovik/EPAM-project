acfc0507f38a66f0ef181c0b1095f80f1ad19cd2
unknown <leontyloz02@gmail.com>
2022-01-10 15:57:41 +0300

    Add FileCabinetService.cs file.

---
+++ FileCabinetApp/FileCabinetService.cs
@@ -0,0 +1,25 @@
+﻿namespace FileCabinetApp
+{
+    public class FileCabinetService
+    {
+        private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
+
+        public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            // TODO: добавьте реализацию метода
+            return 0;
+        }
+
+        public FileCabinetRecord[] GetRecords()
+        {
+            // TODO: добавьте реализацию метода
+            return new FileCabinetRecord[] { };
+        }
+
+        public int GetStat()
+        {
+            // TODO: добавьте реализацию метода
+            return 0;
+        }
+    }
+}
