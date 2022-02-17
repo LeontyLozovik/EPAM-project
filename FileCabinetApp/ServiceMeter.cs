6a5768801392bed358c51101d20b9a6e15ec2619
Lyavontsiy <leontyloz02@gmail.com>
2022-02-17 19:08:00 +0300

    Create new ServiceMeter class using Decorator pattern.

---
+++ FileCabinetApp/ServiceMeter.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// Wrapper for FileCabinet services for measuring run time.
+    /// </summary>
+    public class ServiceMeter : IFileCabinetService
+    {
+        private IFileCabinetService service;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceMeter"/> class.
+        /// </summary>
+        /// <param name="service">one of FileCabinet services.</param>
+        public ServiceMeter(IFileCabinetService service)
+        {
+            this.service = service;
+        }
+
+        /// <summary>
+        /// Create a new record with inputed parameters.
+        /// </summary>
+        /// <param name="record">Record to create.</param>
+        /// <returns>Id of created record.</returns>
+        public int CreateRecord(FileCabinetRecord record)
+        {
+            return this.service.CreateRecord(record);
+        }
+
+        /// <summary>
+        /// Difragment file with records in FileCabinetFilesystemService.
+        /// </summary>
+        /// <returns>Number of difragmented records.</returns>
+        public int Defragment()
+        {
+            return this.service.Defragment();
+        }
+
+        /// <summary>
+        /// Edit record by entered Id.
+        /// </summary>
+        /// <param name="newRecord">New record that replace old record.</param>
+        public void EditRecord(FileCabinetRecord newRecord)
+        {
+            this.service.EditRecord(newRecord);
+        }
+
+        /// <summary>
+        /// Find all records with entered dateofbirth.
+        /// </summary>
+        /// <param name="birthday">dateofbirth to find.</param>
+        /// <returns>all records with entered dateofbirth.</returns>
+        public ReadOnlyCollection<FileCabinetRecord> FindByBirthday(string birthday)
+        {
+            return this.service.FindByBirthday(birthday);
+        }
+
+        /// <summary>
+        /// Find all records with entered firstname.
+        /// </summary>
+        /// <param name="firstName">firstname to find.</param>
+        /// <returns>all records with entered firstname.</returns>
+        public ReadOnlyCollection<FileCabinetRecord> FindByFirstName(string firstName)
+        {
+            return this.service.FindByFirstName(firstName);
+        }
+
+        /// <summary>
+        /// Find all records with entered lastname.
+        /// </summary>
+        /// <param name="lastName">lastname to find.</param>
+        /// <returns>all records with entered lastname.</returns>
+        public ReadOnlyCollection<FileCabinetRecord> FindByLastName(string lastName)
+        {
+            return this.service.FindByLastName(lastName);
+        }
+
+        /// <summary>
+        /// Return all existing records.
+        /// </summary>
+        /// <returns>all existing records.</returns>
+        public ReadOnlyCollection<FileCabinetRecord> GetRecords()
+        {
+            return this.service.GetRecords();
+        }
+
+        /// <summary>
+        /// Return number of existing records.
+        /// </summary>
+        /// <returns>number of existing records.</returns>
+        /// <param name="writeNumberRemoverRecords">Write or don't write number of removedrecords.</param>
+        public int GetStat(bool writeNumberRemoverRecords = true)
+        {
+            return this.service.GetStat(writeNumberRemoverRecords);
+        }
+
+        /// <summary>
+        /// Check if currient Id exist.
+        /// </summary>
+        /// <param name="id">Id to check.</param>
+        /// <returns>True if record with this id exist, false if not exist.</returns>
+        public bool IsIdExist(int id)
+        {
+            return this.service.IsIdExist(id);
+        }
+
+        /// <summary>
+        /// Make a snapshot of current state.
+        /// </summary>
+        /// <returns>instance of FileCabinetServiceSnapshot class.</returns>
+        public FileCabinetServiceSnapshot MakeSnapshot()
+        {
+            return this.service.MakeSnapshot();
+        }
+
+        /// <summary>
+        /// Remove record from service.
+        /// </summary>
+        /// <param name="recordId">Id of record to remove.</param>
+        public void Remove(int recordId)
+        {
+            this.service.Remove(recordId);
+        }
+
+        /// <summary>
+        /// Add records from to service.
+        /// </summary>
+        /// <param name="snapshot">item of FileCabinetServiceSnapshot were records read.</param>
+        /// <returns>number of imported records.</returns>
+        public int Restore(FileCabinetServiceSnapshot snapshot)
+        {
+            return this.service.Restore(snapshot);
+        }
+    }
+}
