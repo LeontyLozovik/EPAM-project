83e6ce81e4e0e410282df820c38dffebfe31e9ab
Lyavontsiy <leontyloz02@gmail.com>
2022-02-24 20:06:35 +0300

    Use yield for MemoryIterator.

--- FileCabinetApp/Iterators/MemoryIterator.cs
+++ FileCabinetApp/Iterators/MemoryIterator.cs
@@ -6,10 +6,9 @@ namespace FileCabinetApp.Iterators
     /// <summary>
     /// Iterrator for FileCabinetMemoryService.
     /// </summary>
-    public class MemoryIterator : IEnumerator, IEnumerable
+    public class MemoryIterator : IEnumerable
     {
         private ReadOnlyCollection<FileCabinetRecord> collection;
-        private int index = -1;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MemoryIterator"/> class.
@@ -21,50 +20,15 @@ namespace FileCabinetApp.Iterators
         }
 
         /// <summary>
-        /// Gets current element of collection.
-        /// </summary>
-        /// <value>Current element of collection.</value>
-        public object Current
-        {
-            get
-            {
-                return this.collection[this.index];
-            }
-        }
-
-        /// <summary>
         /// Returns enumerator.
         /// </summary>
         /// <returns>Enumerator.</returns>
         public IEnumerator GetEnumerator()
         {
-            return this;
-        }
-
-        /// <summary>
-        /// Moves the pointer to the next element.
-        /// </summary>
-        /// <returns>true - if element exist, false if not.</returns>
-        public bool MoveNext()
-        {
-            if (this.index + 1 < this.collection.Count)
+            for (int i = 0; i < this.collection.Count; i++)
             {
-                this.index++;
-                return true;
+                yield return this.collection[i];
             }
-            else
-            {
-                this.Reset();
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Reset index.
-        /// </summary>
-        public void Reset()
-        {
-            this.index = -1;
         }
     }
 }
